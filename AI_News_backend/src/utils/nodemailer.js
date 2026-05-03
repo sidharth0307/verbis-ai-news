6e1bbc627ed8a9c03dddd67e26db941f5b951418
sidharth0307 <sidharthsharma0307@gmail.com>
2026-05-03 19:15:59 +0530

    updated nodemailer

--- AI_News_backend/src/utils/nodemailer.js
+++ AI_News_backend/src/utils/nodemailer.js
@@ -2,16 +2,14 @@ const nodemailer = require("nodemailer");
 
 // reusable transporter with Connection Pooling
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // Use SSL
+  service: "gmail",
+  pool: true, // Crucial for sending multiple newsletter emails efficiently
+  maxConnections: 3,
+  maxMessages: 100,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    pass: process.env.EMAIL_PASS, 
   },
-  tls: {
-    rejectUnauthorized: false
-  }
 });
 
 /**
